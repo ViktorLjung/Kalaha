052c7a1b0f2621bbca47b6a484af40384fe07c2f
Tleety <dahltobbe@hotmail.com>
2016-09-28 15:17:41 +0200

    Started working on the recusive funtion

--- src/ai/MinMax.java
+++ src/ai/MinMax.java
@@ -11,11 +11,27 @@ class Node {
 }
 
 public class MinMax {
+    // Player är vilken spelarnummer vår bot har.
+    private int m_Player;
+    // maxDepth är hur långt ner trädet kommer att söka.
+    private int m_maxDepth;
     
-    public MinMax()
+    public MinMax(int player, int maxDepth)
     {
-        Node node = new Node();
+        // Först initializerar vi våra variabler.
+        m_Player = player;
+        m_maxDepth = maxDepth;
         
-        node.Children.insert(0,)
+        //Skicka in våran node som håller vårat current board state till vår
+        //Rekursiva funktion.
+        
+    }
+    
+    // Player är den spelare som skall göra moven från detta board state.
+    // depth är vilken nivå i trädet vi söker på just nu.
+    public int RecursiveNodeSearch(int player, int depth)
+    {
+        
+        return 
     }
 }
